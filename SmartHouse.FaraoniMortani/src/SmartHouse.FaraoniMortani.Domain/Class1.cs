e15b648ef003a2d779b46ba13cb4b46e351c9ba2
samuelefaraoni <samuele.faraoni.stud@ispascalcomandini.it>
2025-10-29 10:44:39 +0100

    test commit

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartHouse.FaraoniMortani.Domain
+{
+    public class Class1
+    {
+        //Test
+    }
+}
---
+++ SmartHouse.FaraoniMortani/test/SmartHouse.FaraoniMortani.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SmartHouse.FaraoniMortani.Domain.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
