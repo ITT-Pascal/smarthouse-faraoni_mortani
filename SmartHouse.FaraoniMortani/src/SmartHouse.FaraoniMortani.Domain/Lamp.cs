ec61259ea16225fdccb1fce878db838e82d28f66
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2025-10-29 17:30:40 +0100

    Finished Lamp tests but Lamp can be upgraded

--- SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/Lamp.cs
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/Lamp.cs
@@ -2,6 +2,34 @@
 {
     public class Lamp
     {
+
+        // Mode can be: normal, focused and flashing
+        public string? _mode;
         public bool IsOn { get; private set; }
+
+
+        public void TurnOn()
+        {
+            IsOn = true;
+            _mode = "Normal";
+        }
+        
+
+        public void TurnOff()
+        { 
+            IsOn = false;
+        }
+
+
+        public void ChangeMode(string newMode)
+        {
+            // Accepts only the three modes
+            // Can be done different
+            if (newMode == "Normal" || newMode == "Focused" || newMode == "Flashing")
+            {
+                _mode = newMode;
+            }
+        }
+        
     }
 }
--- SmartHouse.FaraoniMortani/test/SmartHouse.FaraoniMortani.Domain.UnitTests/LampTest.cs
+++ SmartHouse.FaraoniMortani/test/SmartHouse.FaraoniMortani.Domain.UnitTests/LampTest.cs
@@ -1,10 +1,102 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace SmartHouse.FaraoniMortani.Domain.UnitTests
 {
     public class LampTest
     {
         [Fact]
-        public void Test1()
+        public void LampTurnOn_WhenIsTurnedOn_IsOnIsTrue()
         {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.TurnOn();
+
+            // Assert
+            Assert.Equal(true, lamp.IsOn);
+        }
+
+        [Fact]
+        public void LampTurnOn_WhenIsTurnedOn_ModeIsNormal()
+        {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.TurnOn();
+
+            // Assert
+            Assert.Equal("Normal", lamp._mode);
+        }
+
+        [Fact]
+        public void LampTurnOff_WhenIsTurnedOff_IsOnIsFalse()
+        {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.TurnOff();
+
+            // Assert
+            Assert.Equal(false, lamp.IsOn);
+
+        }
+
+        [Fact]
+        public void LampChangeMode_WhenTheNewModeIsFocused_ItBecomesFocused()
+        {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.TurnOn();
+            lamp.ChangeMode("Focused");
+
+            //Assert
+            Assert.Equal("Focused", lamp._mode);
+        }
+
+        [Fact]
+        public void LampChangeMode_WhenTheNewModeIsFlashing_ItBecomesFlashing()
+        {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.TurnOn();
+            lamp.ChangeMode("Flashing");
+
+            // Assert
+            Assert.Equal("Flashing", lamp._mode);
+        }
+
+        [Fact]
+        public void LampChangeMode_WhenTheNewModeIsNormal_ItBecomesNormal()
+        {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.TurnOn();
+            lamp.ChangeMode("Normal");
+
+            // Assert
+            Assert.Equal("Normal", lamp._mode);
+        }
+
+        [Fact]
+        public void LampChangeMode_WhenTheNewModeIsNotBetweenNormalFocusedAndFlashing_ItIsNotAssigned()
+        {
+            // Arrange
+            Lamp lamp = new Lamp();
+
+            // Act
+            lamp.ChangeMode("Stefano");
+
+            // Assert
+            Assert.Null(lamp._mode);
 
         }
     }
