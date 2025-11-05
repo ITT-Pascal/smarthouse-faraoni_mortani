497f370530fbfc8705188a00066f8b8e40bb6b5a
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2025-11-05 10:57:52 +0100

    Added TwoLampDeviceTest and upgraded TwolampDevice; TODO: Adjust problem that IsOn is not in abstract class

--- SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/TwoLampDevice.cs
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/TwoLampDevice.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace SmartHouse.FaraoniMortani.Domain
 {
     public class TwoLampDevice
     {
         // TODO: Complete TwoLampDevice
+
+        public AbstractLamp Lamp1 { get; private set; } 
+        public AbstractLamp Lamp2 { get; private set; } 
+
+        public TwoLampDevice(AbstractLamp lamp1, AbstractLamp lamp2)
+        {
+            
+
+        }
+
+        public void TurnBothOn()
+        {
+            if(Lamp1.IsOn)
+            {
+                Lamp1.Switch();
+            }
+
+            if(Lamp2.IsOn)
+            {
+                Lamp2.Switch();
+            }
+        }
+        
+        public void TurnBothOff()
+        {
+            if(!Lamp1.IsOn)
+            {
+                Lamp1.Switch();
+            }
+
+            if(!Lamp2.IsOn)
+            {
+                Lamp2.Switch();
+            }
+        }
     }
 }
---
+++ SmartHouse.FaraoniMortani/test/SmartHouse.FaraoniMortani.Domain.UnitTests/TwoLampDeviceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Domain.UnitTests
+{
+    public class TwoLampDeviceTest
+    {
+        [Fact]
+        public void TurnBothOn_WhenBothLampsGetTurnedOn_BothLampsAreTurnedOn()
+        {
+            // Arrange
+            Lamp lamp1 = new Lamp();
+            Lamp lamp2 = new Lamp();
+            TwoLampDevice twoLampDevice = new TwoLampDevice(lamp1, lamp2);
+
+            // Act
+            twoLampDevice.TurnBothOn();
+
+            // Assert
+
+            Assert.True(lamp1.IsOn);
+            Assert.True(lamp2.IsOn);
+
+        }
+    }
+}
