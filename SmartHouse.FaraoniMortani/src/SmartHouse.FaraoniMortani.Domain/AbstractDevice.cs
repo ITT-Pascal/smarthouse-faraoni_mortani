fdcbb8e2ef0622413c2b764451a59cb01b2f9efd
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2025-11-21 09:51:46 +0100

    Added AbstractDevice, AirConditioner and DeviceStatus

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/AbstractDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Domain
+{
+    public class AbstractDevice
+    {
+
+    }
+}
---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/AirConditioner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Domain
+{
+    public class AirConditioner : AbstractDevice
+    {
+        public const int MinTemperature = 5;
+        public const int DefaultTemperature = 20;
+        public const int MaxTemperature = 40; 
+        public int CurrentTemperature { get; set; }
+        public int TargetTemperature { get; set; }
+
+        public AirConditioner(string name): base(name)
+        {
+            TargetTemperature = DefaultTemperature;
+        }
+
+        public void SetTemperatureToMin()
+        {
+            TargetTemperature = MinTemperature;
+        }
+
+        public void SetTemperatureToDefault()
+        {
+            TargetTemperature = DefaultTemperature;
+        }
+
+        public void SetTemperatureToMax()
+        {
+            TargetTemperature = MaxTemperature;
+        }
+
+
+
+
+
+        
+
+
+    }
+}
+
+
