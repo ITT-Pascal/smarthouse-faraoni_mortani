15437488041e6bb7aab26994190be2274def90d4
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2025-12-19 09:32:47 +0100

    Started added MatrixLamp

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/LuminousDevices/MatrixLamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Domain.LuminousDevices
+{
+    public class MatrixLamp
+    {
+        // Attributes
+        public AbstractLamp[,] matrixLamp { get; private set; }
+
+        // Constructor
+        public MatrixLamp(int rowLength, int columnLength)
+        {
+            matrixLamp = new AbstractLamp[rowLength, columnLength];
+        }
+
+        // Methods        
+        public void AddLamp(AbstractLamp lamp)
+        {
+            
+        }
+    }
+}
