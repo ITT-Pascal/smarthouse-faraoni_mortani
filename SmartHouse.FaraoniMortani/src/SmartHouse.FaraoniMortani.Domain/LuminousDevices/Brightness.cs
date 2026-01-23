5abb73d8a638d67758071568e89dbdc4fd2c70d9
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2026-01-23 09:51:06 +0100

    TODO: finish brightness record

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/LuminousDevices/Brightness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Domain.LuminousDevices
+{
+    public record Brightness
+    {
+        public int Value { get; }
+
+        public const int Min = 0;
+        public const int Max = 100;
+
+        public Brightness(int value)
+        {
+            Value = Math.Clamp(value, Min, Max);
+        }
+
+        public static Brightness operator +(Brightness b, int amount)
+        {
+            return new(b.Value + amount);
+        }
+        
+        // TODO: Finish Brightness record
+    }
+}
