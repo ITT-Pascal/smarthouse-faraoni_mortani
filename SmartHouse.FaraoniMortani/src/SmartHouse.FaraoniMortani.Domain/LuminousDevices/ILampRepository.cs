556f04b7ca87a031f52c725b1bc6f12924e12d03
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2026-02-13 09:47:44 +0100

    Added the application layer

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Domain/LuminousDevices/ILampRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Domain.LuminousDevices
+{
+    public interface ILampRepository
+    {
+        void Create(Lamp newLamp);  
+
+        void Update(Lamp newLamp);
+
+        void Delete(Lamp newLamp);
+
+        Lamp GetById(Guid id);
+
+        List<Lamp> GetAll();
+    }
+}
