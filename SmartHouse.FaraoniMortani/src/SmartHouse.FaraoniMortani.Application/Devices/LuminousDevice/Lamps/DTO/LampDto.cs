83c41da7c78175fb4640ecee92f36f3b6a84d8df
albertomortani <alberto.mortani.stud@ispascalcomandini.it>
2026-02-20 09:50:13 +0100

    Added LampDto and LampMapper

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Application/Devices/LuminousDevice/Lamps/DTO/LampDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Application.Devices.LuminousDevice.Lamps.DTO
+{
+    public class LampDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int Brightness { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime LastModifiedAtUtc { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"Id: {Id}\n" +
+                $"Name: {Name}\n" +
+                $"Status: {Status}\n" +
+                $"Brightness: {Brightness}\n" +
+                $"Created: {CreatedAtUtc}\n" +
+                $"Last update: {LastModifiedAtUtc}\n";
+
+        }
+    }
+}
---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Application/Devices/LuminousDevice/Lamps/Mapper/LampMapper.cs
@@ -0,0 +1,25 @@
+﻿using SmartHouse.FaraoniMortani.Application.Devices.LuminousDevice.Lamps.DTO;
+using SmartHouse.FaraoniMortani.Domain.Devices.LuminousDevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Application.Devices.LuminousDevice.Lamps.Mapper
+{
+    public class LampMapper
+    {
+        public static LampDto ToDto(Lamp lamp)
+        {
+            return new LampDto
+            {
+                Id = lamp.Id,
+                Name = lamp.Name,
+                Brightness = lamp.Brightness.Value,
+                CreatedAtUtc = lamp.CreatedAtUtc,
+                LastModifiedAtUtc = lamp.LastModifiedAtUtc,
+            };
+        }
+    }
+}
