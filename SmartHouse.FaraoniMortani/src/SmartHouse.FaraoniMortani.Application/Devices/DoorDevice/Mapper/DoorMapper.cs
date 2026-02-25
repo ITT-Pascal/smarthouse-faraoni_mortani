c14a6256634b9fe35333f6160bb977016fbf7fcd
samuelefaraoni <samuele.faraoni.stud@ispascalcomandini.it>
2026-02-25 10:26:07 +0100

    Added Door DTO and Mapper

---
+++ SmartHouse.FaraoniMortani/src/SmartHouse.FaraoniMortani.Application/Devices/DoorDevice/Mapper/DoorMapper.cs
@@ -0,0 +1,26 @@
+﻿using SmartHouse.FaraoniMortani.Application.Devices.DoorDevice.DTO;
+using SmartHouse.FaraoniMortani.Domain.Devices.DoorDevice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.FaraoniMortani.Application.Devices.DoorDevice.Mapper
+{
+    public class DoorMapper
+    {
+        public static DoorDto ToDto(Domain.Devices.DoorDevice.Door door)
+        {
+            return new DoorDto
+            {
+                Id = door.Id,
+                Name = door.Name,
+                Password = door.Password,
+                IsLocked = door.IsLocked,
+                CreatedAtUtc = door.CreationTime,
+                LastModifiedAtUtc = door.LastChangeTime,
+            };
+        }
+    }
+}
